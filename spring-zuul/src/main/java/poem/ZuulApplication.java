d6e37daa4326f4e83343cc31bfc7388e55c2c9a3
DESKTOP-6092VH4\曹莉 <caol@dgsdata.com>
2019-02-15 17:43:11 +0800

    添加zuul

---
+++ spring-zuul/src/main/java/poem/ZuulApplication.java
@@ -0,0 +1,18 @@
+package poem;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author 曹莉
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
---
+++ src/main/java/org/poem/SpringZuulApplication.java
@@ -0,0 +1,14 @@
+package org.poem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZuulApplication.class, args);
+    }
+
+}
+
